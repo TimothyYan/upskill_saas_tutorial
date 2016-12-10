0e23d07c5bd78aafad8f2ecffa25d6f0b07204a1
Tim Yan <tyan2@buffalo.edu>
2016-12-10 21:38:51 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,11 @@ class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end
+  
+  def create
+  
+  end
+  
 end
