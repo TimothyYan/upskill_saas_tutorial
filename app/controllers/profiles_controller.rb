93e556d4c44f834a731190d58b80f1394fe5fad4
Tim Yan <tyan2@buffalo.edu>
2016-12-11 00:55:28 +0000

    Added file field for image upload profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -23,6 +23,6 @@ class ProfilesController < ApplicationController
   # White listing form fields, so hacker can't inject malicious code into db
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
 end
